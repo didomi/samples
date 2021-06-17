25b691a4ff149c1e50439d4d81d858487b7cb07e
dvdtsr <tessier.dvd@gmail.com>
2021-06-17 15:57:57 +0200

    indent comments, added ind.html (original state), typos, and style

--- how-to-embed-youtube/script/script.js
+++ how-to-embed-youtube/script/script.js
@@ -2,16 +2,16 @@
 
 function playVideoAndHideOverlay(overlay) {
 
-// Get the youtube iframe with a 'data-src' attribute
+  // Get the youtube iframe with a 'data-src' attribute
   var iframe = overlay.querySelector('iframe[data-src]');
 
-// Get the 'data-src' value
+  // Get the 'data-src' value
   var src = iframe.getAttribute('data-src');
 
-// Set the 'data-src' value to the 'src' attribute
+  // Set the 'data-src' value to the 'src' attribute
   iframe.setAttribute('src', src);
 
-// Hide the overlay
+  // Hide the overlay
   overlay.querySelector('.video-consent-overlay').style.display = 'none';
 }
 
@@ -19,19 +19,19 @@ function playVideoAndHideOverlay(overlay) {
 
 function setPositiveConsentStatusForVendor(vendorId) {
 
-// Get all the vendor purposes
+  // Get all the vendor purposes
   var purposes = Didomi.getVendorById(vendorId).purposeIds;
 
-// Create a "transaction"...
+  // Create a "transaction"...
   var transaction = Didomi.openTransaction();
 
-// ... enable the vendor
+  // ... enable the vendor
   transaction.enableVendor(vendorId);
 
-// ... and all his purposes
+  // ... and all his purposes
   transaction.enablePurposes(...purposes);
 
-// send the new status using "commit"
+  // send the new status using "commit"
   transaction.commit();
 
 }
@@ -41,30 +41,30 @@ function setPositiveConsentStatusForVendor(vendorId) {
 window.didomiOnReady = window.didomiOnReady || [];
 window.didomiOnReady.push(function (Didomi) {
 
-// Subscribe to the vendor status : It triggers the listener each time the status is changed for this vendor.
+  // Subscribe to the vendor status : It triggers the listener each time the status is changed for this vendor.
   Didomi.getObservableOnUserConsentStatusForVendor('c:youtube')
-    .subscribe(function (consentStatus) {
+  .subscribe(function (consentStatus) {
 
-// Check if the "consentStatus" is true (eg. the user agreed to the vendor & his purposes)
-      if (consentStatus === true) {
+    // Check if the "consentStatus" is true (eg. the user agreed to the vendor & his purposes)
+    if (consentStatus === true) {
 
-// Loop into all the ".youtube-container" (even if we only have one in the example)
-          document.querySelectorAll('.youtube-container').forEach(function(video) {
+      // Loop into all the ".youtube-container" (even if we only have one in the example)
+      document.querySelectorAll('.youtube-container').forEach(function(video) {
 
-// call our play & hide function
-            playVideoAndHideOverlay(video);
-
-          })
-
-        }
+        // call our play & hide function
+        playVideoAndHideOverlay(video);
 
       })
 
-// An event listener is attached to each button element
+    }
+
+  })
+
+  // An event listener is attached to each button element
   document.querySelectorAll('.video-consent-overlay-accept-button').forEach(function(button) {
     button.addEventListener('click', function() {
 
-// When the button is clicked, we call the setPositiveConsentStatusForVendor custom function to enable the vendor (Youtube) and all his purposes.
+      // When the button is clicked, we call the setPositiveConsentStatusForVendor custom function to enable the vendor (Youtube) and all his purposes.
       setPositiveConsentStatusForVendor('c:youtube');
 
     })
