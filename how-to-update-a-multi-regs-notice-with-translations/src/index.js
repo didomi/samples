b8841603f26408653abc36e8fa93dbfa135e05bc
Pierre Vanoverschelde <pierre.vano@gmail.com>
2025-05-22 21:57:45 +0200

    refactor: update gitignore

---
+++ how-to-update-a-multi-regs-notice-with-translations/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  console.log(
+    "Didomi - Multi-Regulation Notice Mass Updates Handler\n\n",
+    "Usage:\n\n",
+    "npm run [notice|purposes]:[pull|push|macros] --language= --filename=\n\n",
+    "Example:\n\n",
+    "npm run notice:push --language=es --filename=notice_translations_es.json\n",
+  );
+}
+
+main();
